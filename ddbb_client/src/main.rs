f3301b56edf58c077772133f35375afd5c4cd032
Mingxue-Zhang <mxzhang98@outlook.com>
2023-03-03 08:08:56 +0100

    CLI init

--- ddbb_client/src/main.rs
+++ ddbb_client/src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("This is ddbb client!");
+    let env_args = env::args().collect();
+    let args = argrments::parse_args(env_args);
+    if let Some(opt) = args {
+        println!("我的参数 {:#?}", opt)
+    }
 }
