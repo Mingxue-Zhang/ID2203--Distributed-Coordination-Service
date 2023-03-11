649423c7daa34327004b5bae5aa4b9699de1bfa8
TIM-VtingFU <tim.han.0000@gmail.com>
2023-03-11 21:45:41 +0000

    test serialize ble message

--- ddbb_server/src/main.rs
+++ ddbb_server/src/main.rs
@@ -1,4 +1,15 @@
+#![allow(unused)]
+use omnipaxos_core::messages::ballot_leader_election::BLEMessage;
+use serde_json::Result;
 
 fn main() {
+    let ble = BLEMessage{
+        from: 1,
+        to: 2,
+        msg: omnipaxos_core::messages::ballot_leader_election::HeartbeatMsg::Request(omnipaxos_core::messages::ballot_leader_election::HeartbeatRequest { round: 1 })
+    };
+    let serialized_ble = serde_json::to_vec(&ble).unwrap_or([0].to_vec());
+    let deserialize_ble: BLEMessage = serde_json::from_slice(&serialized_ble[..]).unwrap();
     println!("Hello, world!");
+    println!("{:?}", deserialize_ble);
 }
